d6c1430323d20e53064ab408078572c5f5487004
YarikDolbiev1 <90912350+YarikDolbiev1@users.noreply.github.com>
2021-10-22 20:19:34 +0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,19 @@
+let questions = [
+  "Что мне взять с собой на каток?",
+  "Что мне положить в холодильник?",
+  "Что мне взять с собой в школу?",
+  "Что мне взять с собой в бассейн?",
+];
+let answers = ["коньки", "яблоки", "учебники", "полотенце"];
+
+let question = questions[Math.floor(Math.random()*questions.length)];
+let answer = answers[Math.floor(Math.random()*answers.length)];
+function randomArrayValue(question) {
+      return question[question,length]-1;
+
+}
+function randomArrayValue(answer) {
+  return answer[question,length]-1;
+
+}
+alert(question + ": " + answer);
